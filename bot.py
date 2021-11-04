41918fd4fe46187c814747c5c5ccd8784657bd95
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-11-04 20:34:41 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@
 import datetime
 import hashlib
 import string
-import os
+import uuid
+import os 
+import speech_recognition as sr
 import random
 import requests
 import json
@@ -12,8 +14,38 @@ import telebot
 from telebot import types
 import config
 
+language='ru_RU'
 TELEGRAM_API = os.environ["telegram_token"]
 bot = telebot.TeleBot(TELEGRAM_API)
+
+r = sr.Recognizer()
+
+def recognise(filename):
+    with sr.AudioFile(filename) as source:
+        audio_text = r.listen(source)
+        try:
+            text = r.recognize_google(audio_text,language=language)
+            print('Converting audio transcripts into text ...')
+            print(text)
+            return text
+        except:
+            print('Sorry.. run again...')
+            return "Sorry.. run again..."
+
+@bot.message_handler(content_types=['voice'])
+def voice_processing(message):
+    filename = str(uuid.uuid4())
+    file_name_full="./voice/"+filename+".ogg"
+    file_name_full_converted="./ready/"+filename+".wav"
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
+    text=recognise(file_name_full_converted)
+    bot.reply_to(message, text)
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
 	
 
 @bot.message_handler(commands=["start"])
