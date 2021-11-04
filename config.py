329c3fa9fd3b59259dace085322912a6dd333313
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-11-04 21:17:37 +1000

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,14 @@ url = "https://khvtripbot.herokuapp.com/bot"
 
 # Имя бота (то, которое @)
 bot_name = "khvtripbot"
+
+YANDEX_KEY = 'ce2ee061-4d3f-40aa-adfe-6af946fd65a4'
+
+# https://tech.yandex.ru/speechkit/cloud/doc/dg/concepts/speechkit-dg-overview-technology-recogn-docpage/
+# Language code for speech recognition. You can use: ru-RU, en-US, uk-UK, tr-TR
+VOICE_LANGUAGE = 'ru-RU'
+
+MAX_MESSAGE_SIZE = 1000 * 50  # in bytes
+MAX_MESSAGE_DURATION = 15  # in seconds
+
+language='ru_RU'
